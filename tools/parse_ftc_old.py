9ef97eab06b3a7fb6d93283a01498a49d79ba502
amywang2020 <amywang2020@gmail.com>
2020-06-28 13:58:14 -0700

    Python script for parsing power values set to leftFront motor

---
+++ tools/parse_ftc_old.py
@@ -0,0 +1,43 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime
+
+def get_time(x):
+    x = x.split(' ')
+    x_s = ' ';
+    x_s = x_s.join(x[:2])
+    #print(x_s)
+    x = datetime.strptime(x_s, '%m-%d  %H:%M:%S.%f')
+    return x;
+
+n = 0
+s = []
+r = []
+file1 = open("2.log")
+for line in file1:
+    if "setMotorPowers leftFront:" in line:
+        t = line.split('setMotorPowers leftFront:');
+        print(t[1])
+        s.append(float(t[1]))
+        r.append(get_time(line))
+
+print('hi')
+
+d = []
+d.append(0)
+length = len(r)
+for i in range(length-1):
+    t = r[i+1] - r[0]
+    d.append(t.seconds + t.microseconds/1000000)
+    print(d[i])
+
+fig, ax = plt.subplots()
+plt.plot(d, s)
+
+print(len(s))
+print(len(d))
+
+ax.grid()
+
+plt.show()
