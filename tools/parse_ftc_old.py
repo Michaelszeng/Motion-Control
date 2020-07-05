7402373120fa8f7d9690511da51d7b216047f7dc
amywang2020 <amywang2020@gmail.com>
2020-07-04 22:50:35 -0700

    Python script for parsing logs

--- tools/parse_ftc_old.py
+++ tools/parse_ftc_old.py
@@ -11,39 +11,47 @@ def get_time(x):
     x = datetime.strptime(x_s, '%m-%d  %H:%M:%S.%f')
     return x;
 
-n = 0
-s = []
-r = []
-file1 = open("2.log")
-for line in file1:
-    if "setMotorPowers leftFront:" in line:
-        t = line.split('setMotorPowers leftFront:');
-        print(t[1])
-        s.append(float(t[1]))
-        r.append(get_time(line))
+def plot_graph(filename, parameter):
+    n = 0
+    s = []
+    r = []
 
-print('hi')
+    file1 = open(filename)
+    for line in file1:
+        if parameter in line:
+            t = line.split(parameter);
+            print(t[1])
+            s.append(float(t[1]))
+            r.append(get_time(line))
 
-d = []
-d.append(0)
-length = len(r)
-for i in range(length-1):
-    t = r[i+1] - r[0]
-    d.append(t.seconds + t.microseconds/1000000)
-    print(d[i])
+    d = []
+    d.append(0)
+    length = len(r)
+    for i in range(length - 1):
+        t = r[i + 1] - r[0]
+        d.append(t.seconds + t.microseconds / 1000000)
+        print(d[i])
 
-fig, ax = plt.subplots()
-ax.plot(d, s)
+    # fig, ax = plt.subplots()
+    # ax.plot(d, s)
+    plt.style.use('ggplot')
 
-ax.set(xlabel='time (s)', ylabel='leftFront power',
-        title='motor power vs. time')
-ax.grid()
+    plt.figure()
+    plt.plot(d, s, label = " ")
+    plt.legend();
+    plt.xlabel('time (s)')
+    plt.ylabel(parameter)
 
-plt.show()
+    plt.show()
 
-print(len(s))
-print(len(d))
+    print(len(s))
+    print(len(d))
 
-ax.grid()
+plot_graph('2.log', 'setMotorPowers leftRear:')
+plot_graph('2.log', 'setMotorPowers leftFront:')
+plot_graph('2.log', 'setMotorPowers rightRear:')
+plot_graph('2.log', 'setMotorPowersrightFront:')
 
-plt.show()
+plot_graph('2.log', 'headingError ')
+plot_graph('2.log', 'xError ')
+plot_graph('2.log', 'yError ')
