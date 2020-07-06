129088e8a1ac2688d4acd6da03c302d1545f3956
dinomaster186 <peterwetherell186@gmail.com>
2020-07-06 09:53:06 -0700

    This is the first Version of Basilis which is my version of Road Runner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/jniSquare.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class jniSquare extends LinearOpMode {
+    public native String  stringFromJNI();
+    static{
+        System.loadLibrary("hello-jni");
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+        long start = System.currentTimeMillis();
+        while (opModeIsActive()) {
+            int time = (int)(System.currentTimeMillis() - start)/1000;
+            telemetry.addData("count", time);
+            telemetry.update();
+        }
+    }
+}
